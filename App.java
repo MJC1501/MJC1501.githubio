3bbb8d44833e010b313d662913ced0902d5f9e26
MJC1501 <macyjculbertson@gmail.com>
2025-09-03 09:46:14 -0600

    Create App.java
    SD Class

---
+++ App.java
@@ -0,0 +1,19 @@
+//MACY CULBERTSON
+//For Programming SD class 
+// output hello world
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String brotherOne="Baier";
+        String sisterOne="Reece";
+        String sisterTwo="Teagan";
+        String sisterThree="Laine";
+        
+        System.out.println("Hello, World!");
+        System.out.println("My name is Macy Culbertson and I am 15 years old.");
+        System.out.println("My mom's name is Ashley and my dad's name is Ben");
+        System.out.println("I have four other siblings named "+sisterOne+", "+brotherOne+", "+sisterTwo+" and "+sisterThree+".");
+    }
+} {
+    
+}
