70c7bdd8b494f95c977eb405a957068c12ffa884
MJC1501 <macyjculbertson@gmail.com>
2025-09-15 11:03:40 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,7 +16,12 @@ public class Main {
       System.out.print(i+" ");
       isEvenNumber(i);
     }
-
+int sum = 0;
+  for (int i = 1; i <= 100; i++) {
+      sum = sum + i;
+}
+        System.out.println("Sum is " + sum);
   }
 }
   
+
