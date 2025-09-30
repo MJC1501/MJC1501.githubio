9cb23e4cefe8cdb45e2440b4218b80f1bcc96927
MJC1501 <macyjculbertson@gmail.com>
2025-09-30 15:19:21 -0600

    SD promgramming

--- Horse.java
+++ Horse.java
@@ -4,25 +4,16 @@
 
 public class Horse {
     
-    
-public static void main(String[] args) {
-    
-   Horse horse1=new Horse("june",1990);
-    
-    Horse horse2=new Horse("harry",2010);
 
-    Horse horse3=new Horse("bobby",2015);
-}
-    
-    
-}
     String name;// current name of the horse
     int birthYear;//year the horse was born
     String[] names; //array of old names of the horse
     int numberofNames=0; //how many old names are there.
-    public Horse (String horseName, int year){ //horse constructor needs its name and birth year.
+   
+    public Horse(String horseName, int year){ //horse constructor needs its name and birth year.
         name=horseName; //assigns the name of the horse to the horseName which was sent in the construtor.
         birthYear=year; //assigns the year to the birthYear that was sent in the constructor;
+        names = new String [10];
     };
     
     public void age (){
@@ -34,13 +25,10 @@ public static void main(String[] args) {
         name=newName;
     };
     public String toString (){
-        return name;
-
-      System.out.println("horse1");  
-       System.out.println("horse2");
-        System.out.println("horse3");
-
-};
+        return name + " born in " + birthYear;
+    }
+        
+}
 
 
 
--- HorseRapper.java
+++ HorseRapper.java
@@ -1,3 +1,14 @@
 public class HorseRapper {
+public static void main(String[] args) {
+    
+   Horse horse1=new Horse("june",1990);
+    
+    Horse horse2=new Horse("harry",2010);
 
+    Horse horse3=new Horse("bobby",2015);
+    horse1.changeName("Strawberry");
+    System.out.println(horse1);
+    System.out.println(horse2);
+    System.out.println(horse3);
+}
 }
